ad1fe0ec346e2fdc7cb25cb4b7789da7bc104bb0
Tom Draper <tomjdraper1@gmail.com>
2022-07-28 15:17:59 +0100

    Solar system

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ class CelestrialBody {
 class Planet extends CelestrialBody {
   constructor(radius, orbitRadius, color, sun) {
     super(radius, color);
-    this.orbitRadius = orbitRadius * dUnits;
+    this.orbitRadius = (orbitRadius + sun.radius) * dUnits;
 
     this.x = 0;
     this.y = 0;
@@ -54,15 +54,40 @@ function setup() {
 
   createCanvas(width, height);
 
-  sun = new Sun(width/2, height/2, 40);
+  // Radius values are relatve to Earth (Earth = 1)
+  sun = new Sun(width/2, height/2, 109);
 
-  let earth = new Planet(10, 200, "#287ab8", sun);
+  // Orbital radius are distances 10^10 meters from Sun
+  let mercury = new Planet(0.4, 5.67, "#c9c8c7", sun);
+  planets.push(mercury);
+
+  let venus = new Planet(0.9, 10.7, "#f2d79e", sun);
+  planets.push(venus);
+  
+  let mars = new Planet(0.5, 20.8, "#c65836", sun);
+  planets.push(mars);
+
+  let earth = new Planet(1, 15.1, "#287ab8", sun);
   planets.push(earth);
+
+  let jupiter = new Planet(11.2, 74.2, "#e3b371", sun);
+  planets.push(jupiter);
+  
+  let saturn = new Planet(9.4, 147.7, "#eac757", sun);
+  planets.push(saturn);
+
+  let uranus = new Planet(4, 294.6, "#eac757", sun);
+  planets.push(uranus);
+  
+  let neptune = new Planet(3.9, 447.5, "#88baf0", sun);
+  planets.push(neptune);
 }
 
 function draw() {
   background("#0f0f0f");
   sun.create()
-  planets[0].create();
-  planets[0].orbit();
+  for (let planet of planets) {
+    planet.create()
+    planet.orbit()
+  }
 }
