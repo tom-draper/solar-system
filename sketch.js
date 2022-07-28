34844ac956edf6f266725f68305ec13cc00bdb50
Tom Draper <tomjdraper1@gmail.com>
2022-07-28 15:26:43 +0100

    Planet speed

--- sketch.js
+++ sketch.js
@@ -13,21 +13,18 @@ class CelestrialBody {
 }
 
 class Planet extends CelestrialBody {
-  constructor(radius, orbitRadius, color, sun) {
+  constructor(radius, orbitRadius, speed, color, sun) {
     super(radius, color);
     this.orbitRadius = (orbitRadius + sun.radius) * dUnits;
 
     this.x = 0;
     this.y = 0;
-    this.orbitAngle = 0; // degrees relative to x axis
+    this.orbitAngle = 0; // Degrees relative to x axis
 
-    // 2000 is an arbitrary animation speed (which also depends on the frame rate)
-    // The -1.5 exponent is due to Kepler's 3rd Law
-    this.orbitAngleDelta = 2000 * Math.pow(this.orbitRadius, -1.5);
+    this.orbitAngleDelta = speed * tUnits * Math.pow(this.orbitRadius, -1.5);
     this.sun = sun;
   }
 
-  
   orbit() {
     this.x = this.sun.x + this.orbitRadius * cos(radians(this.orbitAngle));
     this.y = this.sun.y + this.orbitRadius * sin(radians(this.orbitAngle));
@@ -45,8 +42,8 @@ class Sun extends CelestrialBody {
 
 let sun;
 let planets = [];
-let dUnits = 1;
-let tUnites = 1;
+let dUnits = 2.5;
+let tUnits = 10000;
 
 function setup() {
   let width = 1900;
@@ -58,28 +55,28 @@ function setup() {
   sun = new Sun(width/2, height/2, 109);
 
   // Orbital radius are distances 10^10 meters from Sun
-  let mercury = new Planet(0.4, 5.67, "#c9c8c7", sun);
+  let mercury = new Planet(0.4, 5.67, 1/0.88, "#c9c8c7", sun);
   planets.push(mercury);
 
-  let venus = new Planet(0.9, 10.7, "#f2d79e", sun);
+  let venus = new Planet(0.9, 10.7, 1/2.25, "#f2d79e", sun);
   planets.push(venus);
   
-  let mars = new Planet(0.5, 20.8, "#c65836", sun);
-  planets.push(mars);
-
-  let earth = new Planet(1, 15.1, "#287ab8", sun);
+  let earth = new Planet(1, 15.1, 1/3.65, "#287ab8", sun);
   planets.push(earth);
 
-  let jupiter = new Planet(11.2, 74.2, "#e3b371", sun);
+  let mars = new Planet(0.5, 20.8, 1/6.87, "#c65836", sun);
+  planets.push(mars);
+
+  let jupiter = new Planet(11.2, 74.2, 1/43.33, "#e3b371", sun);
   planets.push(jupiter);
   
-  let saturn = new Planet(9.4, 147.7, "#eac757", sun);
+  let saturn = new Planet(9.4, 147.7, 1/107.59, "#eac757", sun);
   planets.push(saturn);
 
-  let uranus = new Planet(4, 294.6, "#eac757", sun);
+  let uranus = new Planet(4, 294.6, 1/306.87, "#eac757", sun);
   planets.push(uranus);
   
-  let neptune = new Planet(3.9, 447.5, "#88baf0", sun);
+  let neptune = new Planet(3.9, 447.5, 1/601.90, "#88baf0", sun);
   planets.push(neptune);
 }
 
